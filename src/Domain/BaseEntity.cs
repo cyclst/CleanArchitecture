1bf2a47d8be479b9179c6c17572917bec58647ae
Richard Lang <richard.lang@puregym.com>
2023-06-22 09:00:59 +0100

    Set BaseEntity.Id to private setter

--- src/Domain/BaseEntity.cs
+++ src/Domain/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace CleanArchitecture.Domain;
 
 public abstract class BaseEntity
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
     private readonly List<BaseEvent> _domainEvents = new();
 
